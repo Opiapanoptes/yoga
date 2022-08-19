775d99b3bd1300c60eb07420bf2bc9afec4a8d72
Opiapanoptes <111597129+Opiapanoptes@users.noreply.github.com>
2022-08-19 20:38:09 +0100

    Add files via upload

---
+++ strapiNuxt/frontend/components/BlockBuilder.vue
@@ -0,0 +1,30 @@
+<template>
+   <div>
+        <HeroSection v-if="this.blockComponent.__component == 'image.hero-section'" :blockData="this.blockComponent" />
+        <TextSection v-if="this.blockComponent.__component == 'text-content.text-section'" :blockData="this.blockComponent" />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'BlockBuilder',
+    props: {
+        ID: Number,
+        blockComponent: Object
+    },
+    
+    data(){
+        return {
+        }
+    },
+
+    methods: {
+    },
+    mounted(){
+        console.log(this.blockComponent);
+    }
+}
+</script>
+
+
---
+++ strapiNuxt/frontend/pages/blog/_page.vue
@@ -0,0 +1,42 @@
+<template>
+  <BlockBuilder :ID="this.pageID" />
+</template>
+
+<script>
+export default {
+    
+  data(){
+    return {
+      page: this.$route.params.page,
+      pageID:''
+    }
+  },
+  methods: {
+    async asyncData() {
+      const thePageID = await fetch(
+          'http://localhost:1337/api/pages?filters[slug]=' + this.page
+          // 'http://localhost:1337/api/pages/1?populate=dynamic_content'
+      ).then((res) => {
+        // can set up 404 redirection here
+      return res.json();
+      });
+      var pageID = thePageID.data[0].id;
+
+      const thePageData = await fetch(
+          'http://localhost:1337/api/pages/' + pageID + '?populate[dynamic_content][populate]=*'
+        ).then((res) => {
+        // can set up 404 redirection here
+        return res.json();
+      });
+
+      console.log('thePageData', thePageData);
+      // console.log(this.$router.params);
+    }
+  },
+
+  mounted(){
+    this.asyncData();
+  }
+  
+}
+</script>
